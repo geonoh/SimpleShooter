d8c33a1f46d7003a19f99f94716d128c46e90f31
Geon Oh <ohhh123@naver.com>
2025-08-15 15:58:54 +0900

    New map added (Sandbox) and pawn character bp are added

--- Source/SimpleShooterEditor.Target.cs
+++ Source/SimpleShooterEditor.Target.cs
@@ -11,5 +11,6 @@ public class SimpleShooterEditorTarget : TargetRules
 		//DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "SimpleShooter" } );
 		bOverrideBuildEnvironment = true;
+		CppStandard = CppStandardVersion.Cpp20;
 	}
 }
