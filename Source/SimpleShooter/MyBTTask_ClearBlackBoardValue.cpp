cb78e1734c3b55f08d0b8f3cb1378dfdd78a1383
Geon Oh <ohhh123@naver.com>
2025-09-20 14:18:16 +0900

    Add custom bptask
    - which is clear blackboard value

---
+++ Source/SimpleShooter/MyBTTask_ClearBlackBoardValue.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBTTask_ClearBlackBoardValue.h"
+
+UMyBTTask_ClearBlackBoardValue::UMyBTTask_ClearBlackBoardValue()
+{
+	NodeName = TEXT("Clear Blackboard Value");
+}
---
+++ Source/SimpleShooter/MyBTTask_ClearBlackBoardValue.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "MyBTTask_ClearBlackBoardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UMyBTTask_ClearBlackBoardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UMyBTTask_ClearBlackBoardValue();
+};
